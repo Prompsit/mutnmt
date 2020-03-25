608a7178ebbe3985ccce4790d96b4bee6414c309
Sergio <sjarmero@prompsit.com>
2020-03-25 12:27:12 +0100

    Datatables class

---
+++ app/utils/datatables.py
@@ -0,0 +1,40 @@
+from sqlalchemy import desc, asc, or_
+from flask import jsonify
+from sqlalchemy.orm import load_only
+
+class Datatables(object):
+    def __init__(self):
+        self.draw = None
+        self.search = None
+
+    def parse(self, table, columns, request):
+        self.draw = request.form.get('draw')
+        self.search = request.form.get('search[value]')
+        start = request.form.get('start')
+        length = request.form.get('length')
+        order = int(request.form.get('order[0][column]'))
+        dir = request.form.get('order[0][dir]')
+
+        rows = table.query.options(load_only(*columns)) \
+                .order_by(asc(columns[order]) if dir == "asc" else desc(columns[order])) \
+                .limit(length).offset(start) \
+                .all()
+    
+        rows_filtered = None
+        if self.search:
+            rows_filtered = table.query.options(load_only(*columns)) \
+                                .filter(or_(*[c.ilike('%{}%'.format(self.search)) for c in columns])) \
+                                .order_by(asc(columns[order]) if dir == "asc" else desc(columns[order])) \
+                                .limit(length).offset(start) \
+                                .all()
+
+
+        return rows, rows_filtered, self.search
+
+    def response(self, rows, rows_filtered, data):
+        return jsonify({
+            "draw": int(self.draw) + 1,
+            "recordsTotal": len(rows),
+            "recordsFiltered": len(rows_filtered) if self.search else len(rows),
+            "data": data
+        })
