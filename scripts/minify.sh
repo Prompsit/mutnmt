6c75ad8546feea9f2ba468cc75cc8ce89b7bce30
Sergio <sjarmero@prompsit.com>
2020-04-14 09:16:38 +0200

    Fix minify bug

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -13,17 +13,17 @@ cat scss/bootstrap.raw.css | postcss --use autoprefixer --no-map > css/bootstrap
 rm scss/bootstrap.raw.css
 
 # Minify css
-for file in $(ls css -I "*.min*")
+for file in $(find css \( -name "*.*" ! -iname "*.min*" \) -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2-)
 do
     filename=$(echo $file | sed 's/\.[^.]*$//')
-    cat css/$file | python3 -m cssmin > css/$filename.min.css
+    cat $file | python3 -m cssmin > $filename.min.css
 done
 
 # Minify js
-for file in $(ls js -I "*.min*")
+for file in $(find js \( -name "*.*" ! -iname "*.min*" \) -type f -printf "%T@ %p\n" | sort -nr | cut -d\  -f2-)
 do
     filename=$(echo $file | sed 's/\.[^.]*$//')
-    python3 -m jsmin js/$file > js/$filename.min.js
+    python3 -m jsmin $file > $filename.min.js
 done
 
 cd $currentdir
