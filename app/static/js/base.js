d518a613c6ebd47fc6e416cd2510b21876ac2fd8
Sergio <sjarmero@prompsit.com>
2021-02-10 13:48:09 +0100

    Nile resurrect

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
         $(`${$(this).attr("data-target")}`).toggleClass("show");
         $('body').toggleClass("overflow-hidden")
     });
+
+    $(".nile-resurrect-btn").on('click', function(e) {
+        e.preventDefault();
+        $('.tour-guide').removeClass('hidden');
+    });
 });
